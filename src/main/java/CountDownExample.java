0a063112d848d3d6061104d10093b43d77d08a9f
pratmishra <v-pratmishra@expedia.com>
2020-05-09 13:20:04 +0530

    count down latch example

---
+++ src/main/java/CountDownExample.java
@@ -0,0 +1,37 @@
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class CountdownTask implements Runnable {
+
+    private CountDownLatch countDownLatch;
+
+    public CountdownTask(CountDownLatch countDownLatch) {
+        this.countDownLatch = countDownLatch;
+
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(4000);
+            countDownLatch.countDown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class CountDownExample {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(6);
+
+        CountdownTask countdownTask = new CountdownTask(countDownLatch);
+        ExecutorService executorService =  Executors.newFixedThreadPool(6);
+        for(int i =0; i<6;i++){
+        executorService.submit(countdownTask);
+        }
+        countDownLatch.await();
+        System.out.println("Task finished !!");
+    }
+}
